a369920eab5f67cfa065280f8269adb67b54df22
Jigar1992 <jigar.thakkar@tatvasoft.com>
2023-09-17 08:38:45 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string temp = "My First Program";
+            Console.WriteLine("Hello World ! " + temp);
+            Console.ReadLine();
+        }
+    }
+}
